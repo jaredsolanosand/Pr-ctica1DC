aa550c4f3a13d09438ded3b15943ec5537fca61f
jaredsolanosand <158521596+jaredsolanosand@users.noreply.github.com>
2024-02-01 16:01:25 -0600

    Server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// Author: Angel Jared Solano Sandoval - jaredsolanosandoval@gmail.com
+const http = require('http')
+const server = http.createServer((peticion, respuesta) => {
+    respuesta.writeHead(200, {'Content-Type': 'text/plain'})
+    respuesta.end("Hola mundo")
+})
+server.listen(3000)
